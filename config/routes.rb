29c407963ecfce4819658c7ea9f62013f901d209
Michelle <rphmichelle@gmail.com>
2023-06-12 10:12:49 -0400

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'chirps/', to: 'chirp#index'
+  post 'chirps/', to: 'chirps#create'
+  get 'chirps/:id', to: 'chirps#show', as: 'chirp'
+  patch 'chirps/:id', to: 'chirps#update'
+  put 'chirps/:id', to: 'chirps#update'
+  delete 'chirps/:id', to: 'chirps#destroy'
+
+  #resources :chirps, except: [:new, :edit]
 end
+
+  #prefix alias 'chirp' to differentiate between routes ('chirps_url' command references both /chirps and /chirps/:id )
+
+  #resources :chirps
+  #   provides all above methods (including 'create' and 'edit') as well as all proper prefixes
+  
